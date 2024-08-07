4e2d6201097cb61e4a26b5e564859d7c26901eee
Akmal <jasminaaa.20@uom.lk>
2024-08-08 01:07:58 +0530

    set up server at port 8000

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Integrating HTMX with Go")
+	fmt.Println("Hello world from HTMX with Go")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello world from HTMX with Go ")
+		io.WriteString(w, r.Method)
+	})
+	log.Fatal(http.ListenAndServe(":8000", nil)) //Fatal is equivalent to [Print] followed by a call to os.Exit(1)
 }

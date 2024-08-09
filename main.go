d3e655fb148df65b7dc08d19bb18d1600514cf77
Akmal <jasminaaa.20@uom.lk>
2024-08-09 05:33:55 +0530

    Insert index file into the main file

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
 	"log"
 	"net/http"
 )
@@ -11,8 +11,8 @@ func main() {
 	fmt.Println("Hello world from HTMX with Go")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello world from HTMX with Go ")
-		io.WriteString(w, r.Method)
+		tmplt := template.Must(template.ParseFiles("index.html"))
+		tmplt.Execute(w, nil)
 	})
 	log.Fatal(http.ListenAndServe(":8000", nil)) //Fatal is equivalent to [Print] followed by a call to os.Exit(1)
 }
